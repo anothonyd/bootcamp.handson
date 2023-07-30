643c46e4d637d5bb79e71b483e392701e5b00ddb
anothonyd <anthony.dulguime.dev@gmail.com>
2023-07-30 15:34:32 +0800

    add loader

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/App.jsx'
+import './views/styles/App.css'
 import './views/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,16 +1,36 @@
+
+import { useState, useEffect } from 'react';
+import LoaderScreen from "../views/components/LoaderScreen";
 import Navbar from "../views/components/layouts/navbar"
 import Section from "../views/components/layouts/section"
 import Sidenav from "../views/components/layouts/sidenav"
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
 
   return (
-    <div className="bg-blue-200 top-0 relative w-full h-max py-3">
-      <Navbar />
-      <div className="relative flex">
-        <Sidenav />
-        <Section />
-      </div>
+    <div className="relative w-full h-screen">
+      {loading ? (
+        <LoaderScreen />
+
+      ) : (
+
+        <div className="bg-blue-200 top-0 relative w-full h-max py-3">
+          <Navbar />
+          <div className="relative flex">
+            <Sidenav />
+            <Section />
+          </div>
+        </div>
+      )}
     </div>
   )
 }
